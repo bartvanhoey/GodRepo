d416f30808de2d1865a0416e00d54d909fe00c94
ekapituro <ekapituro@gmail.com>
2023-11-14 20:48:36 +0100

    Update

---
+++ Net7/Net7Serilog/SerilogExample/SerilogExample/Program.cs
@@ -0,0 +1,44 @@
+
+using Serilog;
+
+namespace SerilogExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().WriteTo.File("Logs/MyLog-.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+            builder.Host.UseSerilog();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseSerilogRequestLogging();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
